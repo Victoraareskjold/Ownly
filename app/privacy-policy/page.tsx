2320f8776f7b37b4b96bd87707a799c1baf4514a
Victor Aareskjold <victor.aareskjold@icloud.com>
2026-03-18 17:42:07 +0100

    feat: privacy-policy page

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,10 @@
+export default function PrivacyPolicyPage() {
+  return (
+    <div>
+      <div className="max-w-3xl mx-auto py-24">
+        <h1>Privacy-policy</h1>
+        <div id="consentify-privacy-policy"></div>
+      </div>
+    </div>
+  );
+}
